6d21b929e785faf0c252279026dfa87f8ec56d7f
Erik Maraldi <erik.maraldi@gmail.com>
2016-07-27 17:22:42 +0200

    Aggiunta classe LoginPanelController

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,61 @@
 package controller;
 
-public class Controller {
+import view.MainView;
+import view.ViewObserver;
+
+public class Controller implements ViewObserver{
+	//private IModel model;
+	private MainView mainView;
+	
+	/**
+	 * Crea un nuovo Controller senza dare alcun parametro come input.
+	 * 
+	 * @param model
+	 *            il model utilizzato per salvare i dati
+	 */
+	public Controller(/*Tipo Imodel model*/) {
+			//this.model = model;
+	}
+
+	@Override
+	public void exitCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveData(String path) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dataLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dataLoad(String path) {
+		// TODO Auto-generated method stub
+		
+	};
+	
+	/**
+	 * Imposta al controller la {@link greenKiosk.views.MainViewInterface} view
+	 * da osservare.
+	 * 
+	 * @param v
+	 *            La {@link greenKiosk.views.MainViewInterface} da osservare.
+	 */
+	//SET VIEW
+
+	
 
 }
---
+++ src/controller/LoginPanelController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import view.MainView;
+
+public class LoginPanelController {
+	//private nomeImodel model;
+	private MainView mainView; 
+	//private nomeinterfaccialogin view;
+	
+	/**
+	 * Crea un nuovo LoginPanelConrtoller
+	 * 
+	 * @param mainView
+	 *            la view da usare per cambiare il
+	 *            display del pannello corrente
+	 * @param model
+	 *            il model da utilizzare
+	 *            per salvare i dati
+	 */
+	/*public LoginPanelControllerImp (MainViewInterface mainView, nomeImodel model) {
+		this.mainView = mainView;
+		this.model = model;
+	}*/
+	/**
+	 * Imposta al controller la view da osservare
+	 * 
+	 * @param mainView
+	 *            la view da usare per cambiare il
+	 *            display del pannello corrente
+	 * @param model
+	 *            il model da utilizzare
+	 *            per salvare i dati
+	 */
+
+}
