d3e5fc0a0900937b51b582b052e8f066f8e8a350
Mattia Rovinelli <rovi92@hotmail.it>
2016-07-30 12:09:06 +0200

    Rinomina della corrente classeIOModel

--- src/model/IOModel.java
+++ src/model/IOModel.java
@@ -4,10 +4,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 
-public interface ReadWriteModificationFile {
+public interface IOModel {
     /** datogli il file restituisce il contenuto del file suddiviso per righe
      * @throws IOException */
    public Map<Integer,ArrayList<String>> readLineFile(String path) throws IOException;
-   
-  
 }
